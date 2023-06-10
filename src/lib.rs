16cc33787318e00b44bb8131b0b001f616238eb8
klownie <audrickyeu@proton.me>
2023-06-10 15:07:30 +0700

    initial thing

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,19 @@
 use bevy::prelude::*;
-use bevy::window::PresentMode;
+use bevy::window::{PresentMode, WindowResolution};
 use bevy_prototype_lyon::prelude::*;
 
 pub struct Burtle {}
 
 impl Burtle {
-    pub fn new() {
+    pub fn setup(width: f32, height: f32) {
         App::new()
             .add_plugins(DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
                     title: "FLOAT".into(),
-                    resolution: (1920., 1080.).into(),
+                    resolution:
+                        WindowResolution::new(width, height).with_scale_factor_override(1.0),
                     present_mode: PresentMode::AutoVsync,
-                    // Tells wasm to resize the window according to the available canvas
-                    fit_canvas_to_parent: false,
-                    // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-                    prevent_default_event_handling: false,
+
                     /*  window_theme: Some(WindowTheme::Dark), */
                     ..default()
                 }),
@@ -28,22 +26,25 @@ impl Burtle {
 }
 #[derive(Component)]
 pub struct Turtle {
-    horizontale: f32,
-    pen_state: bool,
-    registered_pos: (f32, Vec3),
-    instruction: String,
+    pub size: f32,
+    pub heading: f32,
+    pub speed: f32,
+    pub pen_state: bool,
+    pub registered_pos: (f32, Vec3),
+    pub instruction: String,
 }
 
 impl Turtle {
     pub fn right(&mut self, angle: f32) {
-        self.horizontale += angle
+        self.heading += angle
     }
     pub fn left(&mut self, angle: f32) {
-        self.horizontale -= angle
+        self.heading -= angle
     }
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<&Window>) {
+    let window = windows.single();
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -53,23 +54,24 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 anchor: Default::default(),
                 ..default()
             },
-/*             transform: Transform::from_xyz(window.width() / 3.0, window.height() / 2.0, 0.0), */
-            transform: Transform::from_xyz(0.0, 0.0 , 0.0),
+            transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.0),
             texture: asset_server.load("sprites/turtle.png"),
             ..default()
         },
         Turtle {
-            horizontale: 0.0,
+            size: 100.,
+            heading: 0.,
+            speed: 0.,
             pen_state: false,
             registered_pos: (
                 0.0,
-                Vec3::new( 0.0, 0.0, 0.0),
+                Vec3::new(window.width() / 2., window.height() / 2., 0.0),
             ),
             instruction: String::new(),
         },
     ));
     commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.0),
         ..default()
     });
 }
